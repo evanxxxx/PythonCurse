3d1422721d2e903c7b305db8944ec3bb577cb1e7
EvanNavarro90 <162651296+EvanNavarro90@users.noreply.github.com>
2024-05-02 21:27:35 -0600

    up

---
+++ Ciclos/example 1.py
@@ -0,0 +1,16 @@
+""""
+x=""
+for y in range(5):
+    x = x + "*"
+    print(x)
+"""
+
+x=""
+p=""
+for y in range(5):
+    x = x + "*"
+    print(x)
+    for z in reversed.range(5):
+        p = p + "-"
+        print(p)
+
