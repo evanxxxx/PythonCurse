d0ca5538758d9e60691a15c07538b4a5c2c2813c
evanxxxx <evan.navarro.lopez@gmail.com>
2024-05-03 21:38:43 -0600

    up

--- Ciclos/example 1.py
+++ Ciclos/example 1.py
@@ -5,12 +5,40 @@ for y in range(5):
     print(x)
 """
 
-x=""
-p=""
-for y in range(5):
-    x = x + "*"
-    print(x)
-    for z in reversed.range(5):
-        p = p + "-"
-        print(p)
+"""a = [1,2,3]
+b = [0,20,4]
 
+d = a + b
+
+a.extend(b)
+#a.sort()
+
+print(a)"""
+
+"""my_dict = {
+    "name":"evan", "last name":"Navarro", "phone":"5580368040"
+}
+print(my_dict["name"])
+
+for x in my_dict.values():
+    print(x)
+
+for x in my_dict.values():
+    if x == "evann":
+        print(True)
+    else: 
+        print(False)
+
+print("evan" in my_dict.values())
+print(my_dict.keys())"""
+
+my_list=[1,2,3,4,5,6,7,8,9,0]
+
+if 2 not in my_list:
+    print("el 2 no esta en la lista")
+else :
+    print ("el dos si esta en la lista")
+
+for num in my_list:
+    
+    print(num)
